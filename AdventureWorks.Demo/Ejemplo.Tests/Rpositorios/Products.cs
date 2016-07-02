53b202630428fd86508c471de41ff97ed4083e1d
nana1717 <nachamocr17@gmail.com>
2016-07-02 16:05:24 -0600

    Semana8

---
+++ AdventureWorks.Demo/Ejemplo.Tests/Rpositorios/Products.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ejemplo.Tests
+{
+    [TestClass]
+    public class Products
+    {
+        [TestMethod]
+        public void ConsultarProduntosNegros()
+        {
+            //Definir el escenario de la prueba
+            string miColor = "Black";
+            //Repositorio
+            var repositorio = new Ejemplo.BL.Repositorios.ProductRepository();
+            //Invocar al metodo correspondiente
+            var resultado = repositorio
+        }
+    }
+}
